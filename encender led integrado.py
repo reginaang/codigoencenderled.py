9c063cd49b98a15b8de7bbe917e48e8ecdb02476
reginaang <angyregy07@gmail.com>
2024-11-03 02:53:18 -0600

    Create encender led integrado.py
    nomas para el esp32

---
+++ encender led integrado.py
@@ -0,0 +1,10 @@
+import machine
+import time
+
+led = machine.Pin(2, machine.Pin.OUT)  # Configura el LED en el pin 2
+
+while True:
+    led.on()   # Enciende el LED
+    time.sleep(1)  # Espera 1 segundo
+    led.off()  # Apaga el LED
+    time.sleep(1)  # Espera 1 segundo
